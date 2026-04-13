b27b10cdc725a21a09c898d290171b02a1511e20
Yug <savaliyayug85@gmail.com>
2026-04-13 15:09:33 +0530

    remove run_worker

--- run_worker.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from app.scrapers.indian_scraper import main
-
-if __name__ == "__main__":
-    try:
-        # Run the async Indian scraper on the main thread
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("\nWorker shutting down...")
